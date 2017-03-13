3b52bc090dfc51907ae8a46ac19fa185e95c90ff
Gustavo Hurovich <t-guhuro@microsoft.com>
2017-03-13 11:01:47 -0700

    fixes to build and run script

--- src/MusicStore/buildAndRun.ps1
+++ src/MusicStore/buildAndRun.ps1
@@ -1,1 +1,23 @@
-dotnet restore; dotnet publish -c Release -f netcoreapp10; cd .\bin\Release\netcoreapp1.0\publish\; .\Invoke-Crossgen.ps1; &'C:\Program Files\dotnet\dotnet.exe' .\MusicStore.dll; cd ..\..\..\..  
+dotnet restore; 
+
+if ($LastExitCode -ne 0)
+{
+    throw "dotnet restore failed."
+}
+
+dotnet publish -c Release -f netcoreapp10; 
+
+if ($LastExitCode -ne 0)
+{
+    throw "dotnet publish failed."
+}
+
+cd .\bin\Release\netcoreapp1.0\publish\; 
+
+.\Invoke-Crossgen.ps1; &'C:\Program Files\dotnet\dotnet.exe' .\MusicStore.dll; 
+
+if ($LastExitCode -ne 0)
+{
+    throw "Invoke-Crossgen failed."
+}
+cd ..\..\..\..  
